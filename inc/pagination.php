2f55e5c41fca19f5ac24be386f534f386fcbd454
janteuni <janteuni@student.42.fr>
2015-05-12 23:44:52 +0200

    feat(pagination): change and style paginsation

--- inc/pagination.php
+++ inc/pagination.php
@@ -5,7 +5,7 @@
  * @package ananas
  */
 ?>
-<nav id="nav-below">
-	<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'ananas' ) ); ?></div>
-	<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'ananas' ) ); ?></div>
+<nav class="nav-below">
+	<div class="nav-previous"><?php next_posts_link( __( '<', 'ananas' ) ); ?></div>
+	<div class="nav-next"><?php previous_posts_link( __( '>', 'ananas' ) ); ?></div>
 </nav><!-- #nav-above -->
